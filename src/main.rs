54b7534d21ea2a66f357eb1959d522bdd98c43e9
mostafa <mostafayounis630@gmail.com>
2025-11-04 01:18:42 +0200

    implement REPL

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
+    while true {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut command  = String::new();
     io::stdin().read_line(&mut command).expect("failed to take input");
     println!("{}: command not found", command.trim());
-    
+    }
 }
+    
