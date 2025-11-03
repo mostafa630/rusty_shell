f74bc190d8287efba9e70f31f0f6e59c5e6d8651
mostafa <mostafayounis630@gmail.com>
2025-11-04 01:11:20 +0200

    take input from user

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
+    let mut command  = String::new();
+    io::stdin().read_line(&mut command).expect("failed to take input");
+    println!("{}: command not found", command.trim());
     io::stdout().flush().unwrap();
 }
