e983d143e091a48b14d2d88160d5ff657ad1e38d
mostafa <mostafayounis630@gmail.com>
2025-11-04 01:14:37 +0200

    flush

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
+    io::stdout().flush().unwrap();
     let mut command  = String::new();
     io::stdin().read_line(&mut command).expect("failed to take input");
     println!("{}: command not found", command.trim());
-    io::stdout().flush().unwrap();
+    
 }
