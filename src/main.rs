6b20018d680416601f667ae4ad7d0329ed48218d
mostafa <mostafayounis630@gmail.com>
2025-11-06 23:52:11 +0200

    CLI tests (cli-test-1762465931986)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     while true {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut command  = String::new();
-    io::stdin().read_line(&mut command).expect("failed to take input");
-    println!("{}: command not found", command.trim());
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("failed to take input");
+        if command.trim() == "exit" {
+            process::exit(0);
+        }
+        println!("{}: command not found", command.trim());
     }
 }
-    
